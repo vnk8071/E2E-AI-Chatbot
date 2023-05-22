a5797519bd3f43763de1fc377eb91554c6edc730
Long kute <luckim.long.5@gmail.com>
2023-05-22 15:49:31 +0700

    ADD GPT4All model

---
+++ download_gpt4all.sh
@@ -0,0 +1,8 @@
+dir="./models/"
+if [[ ! -e $dir ]]; then
+    mkdir $dir
+elif [[ ! -d $dir ]]; then
+    echo "$dir already exists but is not a directory" 1>&2
+fi
+wget https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin -P $dir
+echo "Done download GPT4ALL in $dir"
---
+++ llms/__init__.py
@@ -0,0 +1,4 @@
+from .base import LLMBase
+from .gpt4all import GPT4AllModel
+
+__all__ = ["LLMBase", "GPT4AllModel"]
