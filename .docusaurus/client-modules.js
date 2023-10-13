98b93190ee510636a0c97740565a9fa803c13d63
Khoi VN <nguyenkhoi8071@gmail.com>
2023-10-14 00:14:14 +0700

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/macos/projects/E2E-AI-Chatbot/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/macos/projects/E2E-AI-Chatbot/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/macos/projects/E2E-AI-Chatbot/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/macos/projects/E2E-AI-Chatbot/src/css/custom.css'),
-];
